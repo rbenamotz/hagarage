1a62a09b510916bf2526724f42032b035d95b7f0
Roey Benamotz <roeybenamotz@Roeys-MacBook-Pro.local>
2018-07-06 08:41:51 -0400

    Updtaed web server

--- src/common.cpp
+++ src/common.cpp
@@ -3,7 +3,9 @@
 #include <stdio.h>
 #include "user_config.h"
 
+
 String log_buffer[LOG_SIZE];
+unsigned long log_buffer_ms[LOG_SIZE];
 int log_buffer_index = -1;
 
 void setupCommon() {
@@ -14,18 +16,17 @@ void setupCommon() {
 
 void write_to_log(String line, ...) {
   char temp[300];
-  char temp2[350];
   const char* l = line.c_str();
   log_buffer_index++;
   int realIndex = log_buffer_index % LOG_SIZE;
   va_list args;
   va_start(args, l);
   vsprintf(temp,l ,args);
-  sprintf(temp2,"%lu : %s",millis(), temp);
-  log_buffer[realIndex] = temp2;
+  log_buffer[realIndex] = temp;
+  log_buffer_ms[realIndex] = millis();
   va_end(args);
   if (LOG_TO_SERIAL) {
-    Serial.println(temp2);
+    Serial.println(temp);
   }
 }
 
@@ -34,9 +35,12 @@ String read_log_buffer() {
   if (log_buffer_index<=0) {
     return "";
   }
+  unsigned long now = millis();
   for (int i=LOG_SIZE; i>0; i--) {
     int p = i + log_buffer_index;
     int l = p % LOG_SIZE;
+    output+= (now - log_buffer_ms[l]) / 1000;
+    output+= " - ";
     output+=log_buffer[l];
     output+="\n";
   }
